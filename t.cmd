c459cf6e5b4c4468489eb7ef47502cfe51f8338a
Chuck Rector <crr.programmer@gmail.com>
2023-03-16 17:56:58 -0700

    Partial decoder for listing 42 (challenge). in/out/xlat/lea/lds/les/lahf/sahf/pushf/popf/adc/inc/aaa/daa/sbb/aas/das/aam/aad/cbw/cwd/into/clc/cmc/stc/cld/std/cli/sti/hlt/wait

--- t.cmd
+++ t.cmd
@@ -9,7 +9,7 @@ set PART1=%CURRENT_DIRECTORY%perfaware\part1
 if not exist build mkdir build
 pushd build
 
-del sim8086.log
+del sim8086.log >nul
 
 for %%f in (%PART1%\*.asm) do (
     if exist %%~nf.disassembled.asm del %%~nf.disassembled.asm
@@ -21,16 +21,32 @@ for %%f in (%PART1%\*.asm) do (
     rem NOTE(chuck): "errorlevel 1" means 1 *or higher*. "errorlevel 0" isn't what you'd think.
     if errorlevel 1 (
         type %%~nf.disassembled.asm >> sim8086.log 2>&1
-        echo FAIL [stage 1] disassemble %%~nf
+        echo FAIL [stage 1] disassemble %PART1%\%%~nf
     ) else (
+        echo PASS [stage 1] disassemble %PART1%\%%~nf
+    )
+
+    echo. >> sim8086.log 2>&1
+    echo call %NASM% %%~nf.disassembled.asm -o %%~nf >> sim8086.log 2>&1
+    call %NASM% %%~nf.disassembled.asm -o %%~nf >> sim8086.log 2>&1
+
+    if errorlevel 1 (
+        echo FAIL [stage 2] reassemble  build\%%~nf.disassembled.asm
+    ) else (
+        echo PASS [stage 2] reassemble  build\%%~nf.disassembled.asm
+
         echo. >> sim8086.log 2>&1
-        echo call %NASM% %%~nf.disassembled.asm -o %%~nf >> sim8086.log 2>&1
-        call %NASM% %%~nf.disassembled.asm -o %%~nf >> sim8086.log 2>&1
+        echo git diff --no-index --word-diff original.xxd reassembled.xxd >> sim8086.log 2>&1
+        del original.xxd > nul
+        del reassembled.xxd > nul
+        xxd -g 1 %PART1%\%%~nf > original.xxd
+        xxd -g 1 %%~nf > reassembled.xxd
+        call git diff --no-index --word-diff original.xxd reassembled.xxd
 
         if errorlevel 1 (
-            echo FAIL [stage 2] reassemble  %%~nf.disassembled.asm
+            echo FAIL [stage 3] diff        build\%%~nf
         ) else (
-            echo PASS [stage 2] reassemble  %%~nf.disassembled.asm
+            echo PASS [stage 3] diff        build\%%~nf
         )
     )
 )
